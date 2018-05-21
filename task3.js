284e810845aabff4352899200fc62f6f3b148d88
Unknown <aliaksei.yusel@itechart-group.com>
2018-05-21 19:52:58 +0300

    adding task3

---
+++ task3.js
@@ -0,0 +1,29 @@
+var petsAllowedInHogwarts = [
+    "Owl",
+    "Toad",
+    "Hippogriff",
+    "Rat",
+    "Cat"
+];
+
+function result3()
+{
+    for (var i = 0; i < petsAllowedInHogwarts.length; i++)
+    {
+        if (petsAllowedInHogwarts[i] == "Hippogriff")
+            var notAllowed = petsAllowedInHogwarts.splice(i, 1);
+    }
+    console.log("Pets ALLOWED in Hogwarts: " + petsAllowedInHogwarts + ";   NOT ALLOWED: " + notAllowed);
+}
+
+
+
+/*function result3()
+{
+    for (var i = 0; i < petsAllowedInHogwarts.length; i++)
+    {
+        if (petsAllowedInHogwarts[i] == "Hippogriff")
+            delete petsAllowedInHogwarts[i];
+    }
+    console.log(petsAllowedInHogwarts);
+}*/
