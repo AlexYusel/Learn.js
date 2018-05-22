b46b198ca3beaac716273273481c68b9db2e526b
Unknown <aliaksei.yusel@itechart-group.com>
2018-05-22 18:14:16 +0300

    fixed task3.js + added index.html

--- task3.js
+++ task3.js
@@ -10,7 +10,7 @@ function result3()
 {
     for (var i = 0; i < petsAllowedInHogwarts.length; i++)
     {
-        if (petsAllowedInHogwarts[i] == "Hippogriff")
+        if (petsAllowedInHogwarts[i] === "Hippogriff")
             var notAllowed = petsAllowedInHogwarts.splice(i, 1);
     }
     console.log("Pets ALLOWED in Hogwarts: " + petsAllowedInHogwarts + ";   NOT ALLOWED: " + notAllowed);
