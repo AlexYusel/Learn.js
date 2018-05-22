1fbecbc1f1e4c582ed4faf37d2be30d2fe6b3576
Unknown <aliaksei.yusel@itechart-group.com>
2018-05-22 19:17:57 +0300

    fixed task3.js + deleted the comment with the alternative solution

--- task3.js
+++ task3.js
@@ -1,29 +1,21 @@
 var petsAllowedInHogwarts = [
-    "Owl",
-    "Toad",
-    "Hippogriff",
-    "Rat",
-    "Cat"
+	"Owl",
+	"Toad",
+	"Hippogriff",
+	"Rat",
+	"Cat"
 ];
+
+var notAllowed = 0;
 
 function result3()
 {
-    for (var i = 0; i < petsAllowedInHogwarts.length; i++)
-    {
-        if (petsAllowedInHogwarts[i] === "Hippogriff")
-            var notAllowed = petsAllowedInHogwarts.splice(i, 1);
-    }
-    console.log("Pets ALLOWED in Hogwarts: " + petsAllowedInHogwarts + ";   NOT ALLOWED: " + notAllowed);
+	for (var i = 0; i < petsAllowedInHogwarts.length; i++)
+	{
+		if (petsAllowedInHogwarts[i] === "Hippogriff")
+		{
+			notAllowed = petsAllowedInHogwarts.splice(i, 1);
+		}
+	}
+	console.log("Pets ALLOWED in Hogwarts: " + petsAllowedInHogwarts + ";   NOT ALLOWED: " + notAllowed);
 }
-
-
-
-/*function result3()
-{
-    for (var i = 0; i < petsAllowedInHogwarts.length; i++)
-    {
-        if (petsAllowedInHogwarts[i] == "Hippogriff")
-            delete petsAllowedInHogwarts[i];
-    }
-    console.log(petsAllowedInHogwarts);
-}*/
